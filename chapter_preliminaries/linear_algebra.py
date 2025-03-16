e1671f7c020ba31f88c3580240a186ffe1df3110
hhh675597 <huanghaohuan24@mails.ucas.ac.cn>
2025-03-16 22:35:33 +0800

    LinearAlgebraFinished

--- chapter_preliminaries/linear_algebra.py
+++ chapter_preliminaries/linear_algebra.py
@@ -22,7 +22,7 @@ print(A.shape[0]) #输出5, 表示行向量(沿0轴)的长度
 B = torch.tensor([[1, 0, 3], [0, 2, 4], [3, 4, 5]])
 print(B == B.T) #symmetric matrix 满足B == B.T
 #对于矩阵而言，尽管列向量是单个向量默认的方向，但实际中以行向量表示一条数据样本，列对应于不同的属性
-X = torch.arange(24).reshape(2, 3, 4)
+X = torch.arange(24, dtype=torch.float32).reshape(2, 3, 4)
 print(X) #张量
 #按元素二元运算
 B = A.clone() #分配新的内存，将A拷贝给B
@@ -46,4 +46,67 @@ print(A_sum_axis1) #tensor([10., 35., 60., 85.])
 print(A.sum(axis=[0, 1])) #沿着所有行和列对矩阵求和，等价于A.sum()
 
 print(A.mean(), A.sum() / A.numel()) #求所有元素的平均值mean()
-print(A.mean(axis=1), A.sum(axis=1) / A.shape[1]) #指定沿哪个轴求平均值
+print(A.mean(axis=1), A.sum(axis=1) / len(A[1])) #指定沿哪个轴求平均值，前后两种方式等价
+print(len(A)) #输出4？？？这是一个4*5的矩阵
+#非降维求和
+sum_A = A.sum(axis=1, keepdims=True)
+print(sum_A) #tensor([[10.],
+             #        [35.],
+             #        [60.],
+             #        [85.]])在对每行进行求和后仍保持两个轴
+print(A / sum_A) #某个元素在所在该行中占的'比例', 用到了广播机制
+
+print(A.cumsum(axis=0)) #tensor([[ 0.,  1.,  2.,  3.,  4.], cumsum函数不会沿任何轴降低输入张量的维数
+                        #[ 5.,  7.,  9., 11., 13.],
+                        #[15., 18., 21., 24., 27.],
+                        #[30., 34., 38., 42., 46.]]) 沿某个轴计算A元素的累计总和.axis=0表示按**行**计算
+#点积：相同位置的按元素乘积之和
+x = torch.arange(4, dtype=torch.float32) #不加类型：RuntimeError: dot : expected both vectors to have same dtype, but found Long and Float
+y = torch.ones_like(x, dtype=torch.float32)
+print(torch.dot(x, y))
+#矩阵-向量积 Recall: 设A m行n列, 从R^n到R^m的线性映射
+print("matrix-vector product")
+A = A.reshape(5, 4)
+print(A.shape, x.shape)
+print(torch.mv(A, x)) #remark:x的长度必须与A的列数相等
+#矩阵乘法
+print("matrix-matrix multiplication")
+B = torch.tensor([[1, 2, 3],
+                  [0, 1, 0],
+                  [0, 0, 1], 
+                  [1, 0, 0]], dtype=torch.float32)
+print(torch.mm(A, B)) #要求B的行数等于A的列数
+#范数 向量&矩阵
+print("norms")
+u = torch.tensor([3.0, -4.0])
+print(torch.norm(u)) #L2范数，向量所有元素平方和的平方根
+print(torch.abs(u).sum()) #L1范数
+#矩阵的Frobenius范数：矩阵元素平方和的平方根
+print(torch.norm(torch.ones(4, 9))) #输出是tensor(6.) 4行9列的全1矩阵
+
+#####Exercises#####
+print(len(X)) #recall:X是2*3*4的矩阵
+#结论：len(X)总是返回张量X在轴0上的长度
+#tensor([[[ 0,  1,  2,  3],
+#         [ 4,  5,  6,  7],
+#         [ 8,  9, 10, 11]],
+#       [[12, 13, 14, 15],
+#         [16, 17, 18, 19],
+#          [20, 21, 22, 23]]])
+#观察下列按轴求和函数的输出，感觉类似压缩饼干/二向箔/液压机
+print(X.sum(axis=0))
+#tensor([[12, 14, 16, 18],
+#        [20, 22, 24, 26],
+#        [28, 30, 32, 34]])
+print(X.sum(axis=1))
+#tensor([[12, 15, 18, 21],0+4+8,1+5+9,2+6+10,3+7+11
+#        [48, 51, 54, 57]])
+print(X.sum(axis=2))
+#tensor([[ 6, 22, 38], hint:0+1+2+3,4+5+6+7,15+19+23
+ #       [54, 70, 86]])
+print(A / A.sum(axis=1, keepdim=True)) 
+#没有keepdim=True会报错，因为广播机制无法将(5，)变为(5,4)，但可以将(5, 1)变为(5, 4)
+
+print(torch.linalg.norm(X)) #输出65.7571
+#RuntimeError: linalg.vector_norm: Expected a floating point or complex tensor as input. Got Long
+###################
