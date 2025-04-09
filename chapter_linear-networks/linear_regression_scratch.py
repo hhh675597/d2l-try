a1a8903f8fe2efd3ccd1d0e7633a63020db55779
hhh675597 <huanghaohuan24@mails.ucas.ac.cn>
2025-04-10 00:34:34 +0800

    AddSomeNotes

--- chapter_linear-networks/linear_regression_scratch.py
+++ chapter_linear-networks/linear_regression_scratch.py
@@ -25,6 +25,12 @@ def data_iter(batch_size, features, labels):
     for i in range(0, num_examples, batch_size):
         batch_indices = torch.tensor(indices[i : min(i + batch_size, num_examples)]) #每次取出batch_size个索引并转换为张量
         yield features[batch_indices], labels[batch_indices] #利用这些索引从特征张量和标签张量中提取对应批次的数据，用yield返回
+#在 Python 中，yield 用于定义一个生成器函数。这种函数返回一个生成器对象，允许你逐步生成序列中的值，而不是一次性创建整个序列。生成器在迭代过程中会“记住”上一次执行结束的位置，并在下一次调用时从该位置继续执行。
+#以下是 yield 工作原理的细节：
+#暂停函数执行：**当生成器函数调用到 yield 时，会暂停执行，并将当前的值返回给调用者。**
+#保存状态：生成器会保存当前局部变量的状态（如循环中的变量），下次迭代时，从暂停的地方继续执行，而不重新初始化。
+#内存效率：由于一次只生成一个数据项，不会一次性加载所有数据，所以它在处理大型数据集时更高效。 
+
 
 #读取第一个小批量数据集并输出(for test)
 batch_size = 10
@@ -59,19 +65,25 @@ def sgd(params, learning_rate, batch_size):
 #两个超参数，需要手动调整
 learning_rate = 0.3
 num_epochs = 3 #迭代周期个数
+
 net = linreg
 loss = squared_loss
 
 for epoch in range(num_epochs):
-    for X, y in data_iter(batch_size, features, labels):
-        l = loss(net(X, w, b), y)
-        
-        l.sum().backward()
-        sgd([w, b], learning_rate, batch_size)
+#############
+    for X, y in data_iter(batch_size, features, labels): #data_iter遍历数据集产生小批量训练集
+        l = loss(net(X, w, b), y) #小批量样本X, y的损失
 
+        l.sum().backward() #反向传播存储每个参数的梯度g
+        sgd([w, b], learning_rate, batch_size) #(w, b)<-(w, b) - \frac{\eta}{|B|} g, where \eta = learning_rate, |B| = batch_size 
+##############
     with torch.no_grad():
         train_l = loss(net(features, w, b), labels)
         print(f'epoch: {epoch + 1}, loss:{float(train_l.mean()):f}')
+
+#在 Python 中，with 语句用于管理上下文资源，确保在进入和退出代码块时自动执行特定操作。
+#torch.no_grad() 是一个上下文管理器，用于临时关闭自动求导机制
+#两者综合起来：进入代码块时临时关闭求梯度
 
 print(w, '\n', b)
 
